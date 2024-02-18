eec038c36b1b0b56b05d508bf03707c9ed81f7ae
mcheviron <mostafaabdelhameed@outlook.com>
2024-02-18 21:21:54 +0100

    Add logging and recovery middlewares

---
+++ server/middlewares.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func (s *Server) LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s.Info("Received request", slog.String("method", r.Method), slog.String("uri", r.URL.Path))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *Server) RecoverMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				s.Error("Panic recovered", slog.String("message", err.(string)))
+				s.InternalServerError(w)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
