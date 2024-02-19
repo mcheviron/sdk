8ab1c438f64ec9ae4a5c4de8052ef2c8e28e8bd4
mcheviron <mostafaabdelhameed@outlook.com>
2024-02-19 15:12:20 +0100

    Add Keys and Values functions to utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -42,3 +42,19 @@ func FilterMap[S ~[]E, E any, R any](s S, f func(E) (R, error)) []R {
 	}
 	return slices.Clip(result)
 }
+
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
